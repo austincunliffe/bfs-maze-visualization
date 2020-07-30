1c193e5a02979a21235012e32ebec9b2a83056d0
williamrdent <williamrdent@gmail.com>
2020-07-30 13:27:53 -0400

    Mazes, Graph and Node class creation and mazereader method start

---
+++ MazeInterpreter.js
@@ -0,0 +1,68 @@
+class Node {
+    constructor(data, x, y, visited, cameFrom, isPath) {
+        this.data = data;
+        this.x = x;
+        this.y = y;
+        this.visited = visited;
+        this.cameFrom = cameFrom;
+        this.isPath = isPath;
+    }
+}
+
+
+let testNode = new Node();
+
+
+class Graph {
+    constructor(nodes, start, goal, sizeX, sizeY) {
+        this.nodes = nodes;
+        this.start = start;
+        this.goal = goal;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
+
+    getStart(){
+        return this.start;
+    }
+
+}
+
+let testGraph = new Graph();
+
+ function buildMaze(){
+    fin = new FileReader();
+    fin.readAsArrayBuffer()
+ }
+
+// /**
+//  * Returns a Graph containing the maze read in from the scanner.
+//  *
+//  * @param fin Scanner to read input file.
+//  * @return Graph containing the file data.
+//  */
+// public static Graph buildMaze(Scanner fin) {
+//     // Store graph size x and y as ints
+//     String graphSize = fin.nextLine();
+//     String[] graphSizes = graphSize.split(" ");
+//     int x = Integer.parseInt(graphSizes[0]);
+//     int y = Integer.parseInt(graphSizes[1]);
+//
+//     // Create and fill graph with nodes containing maze characters
+//     Graph maze = new Graph(x, y);
+//     for (int i = 0; i < x; i++) {
+//         String currentLine = fin.nextLine();
+//         for (int j = 0; j < y; j++) {
+//             maze.nodes[i][j] = new Node(currentLine.charAt(j));
+//             maze.nodes[i][j].x = i;
+//             maze.nodes[i][j].y = j;
+//
+//             // Store the maze start coordinates in the graph
+//             if (maze.nodes[i][j].data == 'S') {
+//                 maze.start[0] = i;
+//                 maze.start[1] = j;
+//             }
+//         }
+//     }
+//     return maze;
+// }
