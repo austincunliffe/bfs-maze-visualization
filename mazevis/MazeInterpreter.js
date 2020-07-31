006ff67380e51b09b4af7cc5c1c0e1316e8f5174
williamrdent <williamrdent@gmail.com>
2020-07-31 14:53:01 -0400

    Maze Vis

--- mazevis/MazeInterpreter.js
+++ mazevis/MazeInterpreter.js
@@ -10,13 +10,11 @@ class Node {
 }
 
 
-let testNode = new Node();
-
 class Graph {
     constructor(nodes, start, goal, sizeX, sizeY) {
         this.nodes = nodes;
         this.start = [];
-        this.goal = goal;
+        this.goal = [];
         this.sizeX = sizeX;
         this.sizeY = sizeY;
     }
@@ -26,9 +24,6 @@ class Graph {
     }
 }
 
-let testGraph = new Graph();
-console.log("Hello from JS");
-
 
 let xhr = new XMLHttpRequest();
 xhr.addEventListener('load', loadData);
@@ -36,11 +31,11 @@ xhr.open('GET', "mazes/bigMaze.txt");
 xhr.send();
 
 function loadData() {
-    console.log("load data");
-    console.log(this);
     let mazeText = this.responseText;
-    // console.log(mazeText);
-    mazeToGraph(mazeText)
+    console.log(mazeText);
+    let graph = mazeToGraph(mazeText);
+    let mazeData = gridData(graph);
+    gridVis(mazeData);
 }
 
 function mazeToGraph(mazeText) {
@@ -48,7 +43,7 @@ function mazeToGraph(mazeText) {
     let mazeLines = new Array();
     for (let line of mazeText.split("\n")) {
         mazeLines.push(line);
-        console.log(line);
+        // console.log(line);
     }
 
 
@@ -64,15 +59,13 @@ function mazeToGraph(mazeText) {
         graph.nodes[i] = [];
     }
 
-
-
     for (let i = 0; i < graph.sizeX; i++) {
         let currentLine = mazeLines[i];
 
         for (let j = 0; j < graph.sizeY; j++) {
             graph.nodes[i][j] = new Node();
-            graph.nodes[i][j].data =currentLine.charAt(j);
-            console.log(currentLine.charAt(j));
+            graph.nodes[i][j].data = currentLine.charAt(j);
+            // console.log(currentLine.charAt(j));
             graph.nodes[i][j].x = i;
             graph.nodes[i][j].y = j;
 
@@ -80,18 +73,99 @@ function mazeToGraph(mazeText) {
                 graph.start.push(i);
                 graph.start.push(j);
             }
+            if (graph.nodes[i][j].data === 'G') {
+                graph.goal.push(i);
+                graph.goal.push(j);
+            }
         }
     }
 
-    console.log(graph.start)
-    console.log(graph.goal)
 
-    for(let i = 0; i<graph.nodes.sizeX;i++) {
-        for (let j=0; j<graph.nodes.sizeY;j++){
-            console.log(graph.nodes[i][j].data)
+    for (let i = 0; i < graph.nodes.sizeX; i++) {
+        for (let j = 0; j < graph.nodes.sizeY; j++) {
+            // console.log(graph.nodes[i][j].data)
         }
     }
-        return graph;
+    return graph;
+}
+
+
+function gridData(graph) {
+    var data = new Array();
+    var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+    var ypos = 1;
+    var width = 20;
+    var height = 20;
+
+    console.log(graph.sizeX)
+    console.log(graph.sizeY)
+
+    for (var row = 0; row < graph.sizeX; row++) {
+        data.push(new Array());
+
+        for (var column = 0; column < graph.sizeY; column++) {
+            data[row].push({
+                x: xpos,
+                y: ypos,
+                cellType: graph.nodes[row][column].data,
+                width: width,
+                height: height
+            })
+            xpos += width;
+        }
+        xpos = 1;
+        ypos += height;
+    }
+    return data;
+}
+
+function gridVis(gridData) {
+    var grid = d3.select("#grid")
+        .append("svg")
+        .attr("width", "1000px")
+        .attr("height", "1000px");
+
+    var row = grid.selectAll(".row")
+        .data(gridData)
+        .enter().append("g")
+        .attr("class", "row");
+
+    var column = row.selectAll(".square")
+        .data(function (d) {
+            return d;
+        })
+        .enter().append("rect")
+        .attr("class", "square")
+        .attr("x", function (d) {
+            return d.x;
+        })
+        .attr("y", function (d) {
+            return d.y;
+        })
+        .attr("width", function (d) {
+            return d.width;
+        })
+        .attr("height", function (d) {
+            return d.height;
+        })
+        .attr("cellType", function (d) {
+            return d.cellType;
+        })
+        .style("fill", function (d) {
+            if (d.cellType == 'X') {
+                return "#000";
+            } else if (d.cellType == 'G') {
+                return "#ff0083";
+            } else if (d.cellType == 'S') {
+                return "#10ff00";
+            } else {
+                return "#fff";
+            }
+        })
+        .style("stroke", "#222")
+
+
+    console.log();
 }
 
 
